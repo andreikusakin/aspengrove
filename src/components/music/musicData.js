a186379443cf091c8ba91b4a42e60a0e81df2f9a
Andrei Kusakin <62080964+andreikusakin@users.noreply.github.com>
2024-01-19 12:33:58 -0500

    Deleted singles

--- src/components/music/musicData.js
+++ src/components/music/musicData.js
@@ -16,59 +16,11 @@ export const musicData = [
     img: "/images/ithinkimyours.jpg"
   },
   {
-    title: "Sound Of A Broken Heart",
-    type: "Single",
-    year: 2021,
-    link: "https://fanlink.to/soundofabrokenheart",
-
-    img: "/images/soundofabrokenheart.jpg"
-  },
-  {
-    title: "Till The End Of Days",
-    type: "Single",
-    year: 2021,
-    link: "https://fanlink.to/tilltheendofdays",
-
-    img: "/images/tilltheendofdays.jpg"
-  },
-  {
-    title: "Don't Fly Away Too Far",
-    type: "Single",
-    year: 2021,
-    link: "https://fanlink.to/dontflyawaytoofarstream",
-
-    img: "/images/dontflyawaytoofar.jpg"
-  },
-  {
-    title: "Northern Star",
-    type: "Single",
-    year: 2022,
-    link: "https://fanlink.to/northernstar",
-
-    img: "/images/northernstar.jpg"
-  },
-  {
-    title: "Golden Sands",
-    type: "Single",
-    year: 2022,
-    link: "https://fanlink.to/goldensands",
-
-    img: "/images/goldensands.jpg"
-  },
-  {
     title: "Mucle Memories",
     type: "Album",
     year: 2022,
     link: "https://fanlink.to/musclememories",
 
     img: "/images/musclememories.jpg"
-  },
-  {
-    title: "Ne zabuvai mene 🇺🇦",
-    type: "Single",
-    year: 2023,
-    link: "https://fanlink.to/nezabuvaimene",
-
-    img: "/images/nezabuvaimene.jpg"
   },
 ];
